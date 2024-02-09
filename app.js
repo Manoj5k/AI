302e32d8d9432b8be49726a20486ab2226392d97
Manoj5k <149101786+Manoj5k@users.noreply.github.com>
2024-02-09 13:10:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ function speakThis(message) {
         speech.text = finalText;
     }
     else if(message.includes('open whatsapp')) {
-        window.open("//whatsapp", "_blank");
+        window.open("//whatsapp.com", "_blank");
         const finalText = "Opening whatsapp";
         speech.text = finalText;
     }
